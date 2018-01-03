87a56a6d8d2e4772309b7d234b9f7fc5e4b1ba76
Marko Esna <maesna@ttu.ee>
2018-01-03 22:22:12 +0200

    Added parsing raw CSV data to price points data.

--- advanced_python/csv_parser.py
+++ advanced_python/csv_parser.py
@@ -1,6 +1,19 @@
+import csv
+from collections import namedtuple
+from datetime import datetime
+
+PricePoint = namedtuple('PricePoint', ['date', 'price'])
+
+
 class CsvParser:
     def __init__(self, file_path):
         self.__file_path = file_path
 
     def parse(self):
         print "Parsing file '{}'.".format(self.__file_path)
+
+        with open(self.__file_path, 'rb') as csv_file:
+            raw_data = [tuple(line) for line in csv.reader(csv_file)]
+
+        price_data = [PricePoint(date=datetime.strptime(date, "%Y-%m-%d %H:%M:%S"),
+                                 price=float(price)) for date, price in raw_data]
