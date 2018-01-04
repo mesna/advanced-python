358f053f5979f6097d1578ce1be3ac7d946eb2a8
Marko Esna <maesna@ttu.ee>
2018-01-04 20:55:52 +0200

    Reordered the calculations operations.

--- advanced_python/number_utils.py
+++ advanced_python/number_utils.py
@@ -5,5 +5,5 @@ class NumberUtils:
     @staticmethod
     def calculate_percentage_increase(start_value, end_value):
         change = end_value - start_value
-        percentage_increase = change * 100.0 / start_value
+        percentage_increase = change / start_value * 100.0
         return percentage_increase
