7b62929a4cca3c9e8dff66d03c2ca83bc6b0ced1
Marko Esna <maesna@ttu.ee>
2018-01-04 20:45:34 +0200

    Added calculation of the greatest increase over previous day.

--- advanced_python/csv_parser.py
+++ advanced_python/csv_parser.py
@@ -2,6 +2,9 @@ import csv
 from collections import namedtuple
 from datetime import datetime
 
+from .number_utils import NumberUtils
+from .info_bit import InfoBit
+
 PricePoint = namedtuple('PricePoint', ['date', 'price'])
 
 
@@ -17,3 +20,23 @@ class CsvParser:
 
         price_data = [PricePoint(date=datetime.strptime(date, "%Y-%m-%d %H:%M:%S"),
                                  price=float(price)) for date, price in raw_data]
+
+        greatest_increase_over_prev_day = self.greatest_increase_over_prev_day(price_data)
+
+    def greatest_increase_over_prev_day(self, price_data):
+        biggest_change = 0
+        date_of_biggest_change = None
+        increase_percentage = 0
+
+        for i in range(len(price_data) - 1):
+            previous = price_data[i].price
+            current = price_data[i + 1].price
+            change = current - previous
+            if change > biggest_change:
+                biggest_change = change
+                date_of_biggest_change = price_data[i + 1].date
+                increase_percentage = NumberUtils.calculate_percentage_increase(previous, current)
+
+        return InfoBit(description="Greatest percent increase over the previous day",
+                       value=increase_percentage,
+                       date=date_of_biggest_change)
--- advanced_python/info_bit.py
+++ advanced_python/info_bit.py
@@ -1,8 +1,8 @@
 class InfoBit:
-    def __init__(self, description, value, day):
+    def __init__(self, description, value, date):
         self.__description = description
         self.__value = value
-        self.__day = day
+        self.__date = date
 
     def description(self):
         return self.__description
@@ -10,5 +10,8 @@ class InfoBit:
     def value(self):
         return self.__value
 
-    def day(self):
-        return self.__day
+    def date(self):
+        return self.__date
+
+    def __str__(self):
+        return "[description={}, value={}, date={}]".format(self.description(), self.__value, self.__date)
